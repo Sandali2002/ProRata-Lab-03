67940e7d76b050104955322f2e5eb4ae59d26d64
Tharuka Nishshanka <sandalitharukan@gmail.com>
2026-09-13 23:39:19 +0530

    Add files via upload

---
+++ Q1B/IT23376472Lab3Q1B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IT23376472Lab3Q1B{
+	
+	public static void main (String[] args){
+		
+		int no = 10;
+		int x = 5;
+		
+		 
+		
+		System.out.println(x++);
+		
+	}
+
+}
